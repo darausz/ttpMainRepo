ec7abbad72bca5cb61aa615ab6541ca8d4643efe
dareaus <darauszhang@gmail.com>
2023-07-10 13:31:28 -0400

    7/10 node lab work

---
+++ work/node_basics_lab/fileA.js
@@ -0,0 +1,12 @@
+// const exportsFromFileB = require('./fileB'); // note the relative file path
+// console.log(exportsFromFileB.foo);
+// console.log(exportsFromFileB.bar);
+
+// exportsFromFileB();
+
+snippet1 = require("./snippet1.js");
+snippet2 = require("./snippet2.js");
+snippet3 = require("./snippet3.js");
+console.log(snippet1);
+console.log(snippet2);
+console.log(snippet3);
