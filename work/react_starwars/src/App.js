1a9e339f94521a5232681a37a0955891b069de52
dareaus <darauszhang@gmail.com>
2023-07-26 14:00:13 -0400

    react star wars lab 7/26

---
+++ work/react_starwars/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import './StarWarsCharacters';
+import StarWarsCharacters from "./StarWarsCharacters";
+
+function App() {
+  return (
+    <StarWarsCharacters />
+  );
+}
+
+export default App;
---
+++ work/react_starwars/src/StarWarsCharacters.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState} from "react";
+import Axios from "axios";
+
+function StarWarsCharacters () {
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        const fetchCharacters = async () => {
+            try {
+                const response = await Axios.get("https://swapi.dev/api/people/");
+                setCharacters(response.data.results);
+            }
+            catch (error) {
+                console.error("error fetching data", error);
+            }
+        }
+        
+        fetchCharacters();
+    }, []);
+
+    return (
+        <div>
+            <h1>Star Wars Characters</h1>
+            <ul>
+                {characters.map((character) => (
+                    <li key={character.name}>
+                        <h2>{character.name}</h2>
+                        <p>Home World: {character.homeworld}</p>
+                        <p>Species: {character.species[0]}</p>
+                        <p>Gender: {character.gender}</p>
+                        <p>Height: {character.height}</p>
+                        <p>Birth Year: {character.birth_year}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default StarWarsCharacters;
